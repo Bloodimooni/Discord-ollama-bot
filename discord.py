5592407e3fe6b6ff4df58e5cd3b72791e2f0b3b0
Joshua <39704584+Bloodimooni@users.noreply.github.com>
2023-11-14 16:11:40 +0100

    build main bot

---
+++ discord.py
@@ -0,0 +1,48 @@
+import discord
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+START = "!ai"
+TOKEN = os.getenv("TOKEN")
+
+client = discord.Client()
+
+def send_to_ollama(message):
+    
+    url = "http://localhost:11434/api/generate"
+
+    data = {
+        "model":"llama2-uncensored",
+        "prompt": message
+    }
+
+    try: 
+        response = requests.post(url, json=data)
+        response.raise_for_status()
+
+        answer = response.json().get("answer", "Sorry, I could not process that.")
+
+        return answer
+
+    except requests.exceptions.RequestException as e:
+
+        return f"An error occured: {e}"
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith(START):
+        ollama_response = send_to_ollama(message.content[len(START):])
+        await message.channel.send(ollama_response)
+
+    
+client.run(TOKEN)
