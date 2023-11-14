51e713597a41d565fc8deacdd25dc64f3973b134
Joshua <39704584+Bloodimooni@users.noreply.github.com>
2023-11-14 23:44:42 +0100

    Update discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -3,6 +3,7 @@ import discord
 import requests
 from dotenv import load_dotenv
 import json
+import aiohttp
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -18,26 +19,28 @@ async def ask_ollama(question,model="llama2-uncensored"):
     print(f"using {model} to answer")
     url = 'http://localhost:11434/api/generate'
     data = {"model": model, "prompt": question}
+    timeout = aiohttp.ClientTimeout(total=120)
 
-    try:
-        response = requests.post(url, json=data)
-        response.raise_for_status()
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        try:
+            async with session.post(url, json=data) as response:
+                response.raise_for_status()
 
-        # Split the response into individual JSON strings
-        json_strings = response.text.strip().split('\n')
+                # Split the response into individual JSON strings
+                json_strings = response.text.strip().split('\n')
 
-        # Parse each JSON string and concatenate the 'response' fields
-        full_response = ''
-        for json_str in json_strings:
-            parsed_json = json.loads(json_str)
-            full_response += parsed_json.get('response', '')
+                # Parse each JSON string and concatenate the 'response' fields
+                full_response = ''
+                for json_str in json_strings:
+                    parsed_json = json.loads(json_str)
+                    full_response += parsed_json.get('response', '')
 
-        return full_response
+                return full_response
 
-    except requests.exceptions.RequestException as e:
-        return f"An error occurred: {e}"
-    except json.JSONDecodeError as e:
-        return f"JSON parsing error: {e}"
+        except requests.exceptions.RequestException as e:
+            return f"An error occurred: {e}"
+        except json.JSONDecodeError as e:
+            return f"JSON parsing error: {e}"
 
 
 @client.event
